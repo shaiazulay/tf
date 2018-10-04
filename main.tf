3e84be6bd757b8fbe3bf3660c688c38d2e55aea6
Shai Azulay <shaiaz@rnd-hub.com>
2018-10-05 01:19:50 +0300

    Working as expected

--- main.tf
+++ main.tf
@@ -6,7 +6,22 @@ provider "mysql" {
 }
 
 # Create a Database
-resource "mysql_database" "app" {
+resource "mysql_database" "databases" {
   count = "${length(var.databases_names)}"
   name = "${var.databases_names[count.index]}"
+}
+
+resource "mysql_user" "users" {
+  count = "${length(keys(var.usernames_passwords))}"
+  plaintext_password =  "${var.usernames_passwords[element(keys(var.usernames_passwords), count.index)]}"
+  host               = "%"
+  user = "${element(keys(var.usernames_passwords), count.index)}"
+}
+
+resource "mysql_grant" "grant_select" {
+  count = "${length(keys(var.usernames_passwords)) * length(var.databases_names)}"
+  user       = "${element(keys(var.usernames_passwords), count.index % length(keys(var.usernames_passwords)))}"
+  host       = "%"
+  database   = "${var.databases_names[count.index % length(var.databases_names)]}"
+  privileges = ["SELECT"]
 }
